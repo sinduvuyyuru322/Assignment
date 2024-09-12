e3126423c9c2af8ccc6396f4d441720f7a1d5b0c
sindu <sinduvuyyuru322@gmail.com>
2024-09-12 13:11:56 +0530

    tuple

---
+++ 1209.py
@@ -0,0 +1,28 @@
+#given a tuple containing nested tuple,write a python program to flatten it into a single tuple
+tuple_1 = eval(input())
+lis = []
+for i in tuple_1:
+    a_str = str(i)
+    if len(a_str) == 1:
+        lis.append(int(a_str))
+    else:
+        a_list = list(i)
+        for i in a_list:
+            lis.append(i)
+print(lis)
+    
+#write a python program to sort a tuple based on the second element of each tuple 
+first_in = eval(input())
+li = []
+for i in first_in:
+    li.append(i[1])
+a = sorted(li)
+empty_lis = []
+for i in a:
+    for j in first_in:
+        list_j = list(j)
+        if list_j[1] == i:
+            if j not in empty_lis:
+                empty_lis.append(j)
+print(tuple(empty_lis))
+
